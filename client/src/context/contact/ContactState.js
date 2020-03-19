98744d2242acff684ed92c982456d7729755128e
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-03-19 08:45:59 +0700

    Set & Clear Current Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,7 +36,8 @@ const ContactState = props => {
         phone: '333-333-333',
         type: 'professional'
       }
-    ]
+    ],
+    current: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -51,9 +52,13 @@ const ContactState = props => {
     dispatch({ type: DELETE_CONTACT, payload: id });
   }
   //Set Current Contact
-
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  }
   //Clear Current Contact
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  }
   //Update Contact
 
   //Filter Contacts
@@ -61,7 +66,7 @@ const ContactState = props => {
   //CLear Filter
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts, addContact, deleteContact }}>
+    <ContactContext.Provider value={{ contacts: state.contacts, current: state.current, addContact, deleteContact, setCurrent, clearCurrent }}>
       {props.children}
     </ContactContext.Provider>
   )
